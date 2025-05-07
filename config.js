139553e3cb47de7618c058a8d4f3b16a9077754d
soowan <nick6767676767@gmail.com>
2025-05-07 14:09:45 +0900

    feat: 로그아웃 기능 구현

--- config.js
+++ config.js
@@ -4,23 +4,21 @@
 module.exports = {
     // Main API proxy settings
     apiProxy: {
-        target: 'http://localhost:8080',
+        target: 'https://dev-linkup.duckdns.org',
         changeOrigin: true,
         pathRewrite: {
-            '^/api': '/v1', // Rewrite /api to /v1 on the target server
+            '^/api': '/v1',
         },
         secure: false, // Set to true if you need HTTPS validation
-        // Add auth headers if needed
         headers: {
             // 'Authorization': 'Bearer YOUR_TOKEN',
         }
     },
 
-    // Additional proxy endpoints can be defined here
     serviceProxies: {
         auth: {
             path: '/auth',
-            target: 'http://localhost:8080',
+            target: 'https://dev-linkup.duckdns.org',
             changeOrigin: true,
             pathRewrite: {
                 '^/auth': '',
@@ -28,7 +26,7 @@ module.exports = {
         },
         uploads: {
             path: '/uploads',
-            target: 'http://localhost:8080',
+            target: 'https://dev-linkup.duckdns.org',
             changeOrigin: true,
             pathRewrite: {
                 '^/uploads': '/files',
