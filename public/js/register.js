e5a5e4ec1adbb5451282244ea25c78a024b0f53f
soowan <nick6767676767@gmail.com>
2025-05-09 16:05:10 +0900

    fix: 직접 링크 전달로 변경

--- public/js/register.js
+++ public/js/register.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const socialLoginButtons = document.querySelectorAll('button.w-full');
 
   socialLoginButtons.forEach(button => {
     button.classList.add('hover-scale');
 
-    button.addEventListener('click', function(e) {
+    button.addEventListener('click', function (e) {
       e.preventDefault();
 
       let provider = '';
@@ -18,12 +18,11 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       if (provider) {
-        // 프론트엔드 서버의 OAuth 요청 라우트로 리다이렉트
-        const url = `/oauth2/authorization/${provider}`;
-        const backendUrl = `${process.env.API_URL}${url}`;  // 템플릿 리터럴 사용
+        
+        const url = `https://backend.linkup.o-r.kr/oauth2/authorization/${provider}`; 
         console.log(`소셜 로그인 요청: ${url}`);
         sessionStorage.setItem('isLoggedIn', 'true');
-        window.location.href = backendUrl;
+        window.location.href = url;
       }
     });
   });
